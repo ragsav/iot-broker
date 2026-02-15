e253b74e6d1aadcc2006b205f7b17dbc24a7aad7
ragsav <raghav227996399@gmail.com>
2026-02-15 20:50:30 +0530

    api notifications

--- services/iot.service.js
+++ services/iot.service.js
@@ -234,8 +234,7 @@ const IOTService = {
         ) VALUES (
             $1, $2, NOW(), $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17,
             $18, $19, $20, $21, $22, $23, $24, $25, $26, $27, $28, $29, $30, $31, $32, $33
-        )
-        ON CONFLICT (iot_imei, iot_timestamp) DO NOTHING;
+        );
       `;
 
       let latestRecord = null;
