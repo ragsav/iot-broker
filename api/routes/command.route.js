39d00e42640646d51629a8e22ea7782dbf4f9da3
ragsav <raghav227996399@gmail.com>
2026-02-15 19:58:46 +0530

    api notifications

--- api/routes/command.route.js
+++ api/routes/command.route.js
@@ -3,6 +3,6 @@ const router = express.Router({ mergeParams: true });
 const {authMiddleware} = require('../middlewares/auth.middleware');
 const { commandController } = require('../controllers/command.controller');
 
-router.post('/v1/commands', authMiddleware.checkApiKey, commandController.executeCommand);
+router.post('/v1/command', authMiddleware.checkApiKey, commandController.executeCommand);
 
 module.exports = {commandRouter:router};
